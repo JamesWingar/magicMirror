1a2a97d1f5d22dae9c02c6c6da0d35504169d6d9
James <james.wingar@gmail.com>
2021-02-02 22:46:03 +0000

    Add constants folder

---
+++ magicMirror/frontend/src/constants/index.js
@@ -0,0 +1,4 @@
+export const API_URL = "http://localhost:8000/api/";
+export const API_DATE_URL = "http://localhost:8000/api/date/";
+export const API_JOB_URL = "http://localhost:8000/api/job/";
+export const API_EVENT_URL = "http://localhost:8000/api/event/";
