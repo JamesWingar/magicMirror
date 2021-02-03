6aeb88bd5f180bee449f2ec637bec80287f25693
James <james.wingar@gmail.com>
2021-02-03 19:52:39 +0000

    Add weather module with required edits to constants folder (API details)

---
+++ magicMirror/frontend/src/components/WeatherModule.js
@@ -0,0 +1,76 @@
+import React from "react";
+import ListGroup from "react-bootstrap/ListGroup";
+import WeatherAPI from "../utility/WeatherAPI";
+import { WEATHER_ICON } from "../constants/index";
+
+class Weather extends React.Component{
+    constructor() {
+        super();
+        this.state = {
+            visible: true,
+            weatherCurrent: [],
+            weatherForecast: [],
+        };
+        WeatherAPI.getWeatherCurrent().then((response) => {
+                this.setState({ weatherCurrent: response.data })
+            })
+        WeatherAPI.getWeatherForecast().then((response) => {
+                this.setState({ weatherForecast: response.data.list.[1] })
+            })
+    }
+
+    componentDidMount() {
+        setInterval(()=>{
+            WeatherAPI.getWeatherCurrent().then((response) => {
+                    this.setState({ weatherCurrent: response.data })
+                })
+            WeatherAPI.getWeatherForecast().then((response) => {
+                    this.setState({ weatherForecast: response.data.list.[1] })
+                })
+        },360000)
+    }
+
+    getWeather(result) {
+        if (result == "Error") {
+            return <div> {result} </div>
+        }
+        if (result.length === 0) {
+            return <div> No data </div>
+        }
+
+        var weather = {
+            temp: (Number(result.main.temp) - 273).toFixed(1),
+            humidity: result.main.humidity,
+            description: result.weather.[0].description,
+            icon: result.weather.[0].icon,
+        }
+
+        return (
+            <div>
+                <div>Temp: {weather.temp}°c</div>
+                <div>Humidity: {weather.humidity}%</div>
+                <div>Description: {weather.description}</div>
+                <img src={"../../static/images/" + WEATHER_ICON[weather.icon] + ".png"} width='100px'/>
+            </div>
+        );
+    }
+
+    render() {
+        const weatherStyle = {
+            textAlign: 'left',
+            color: '#fff',
+            padding: "10px",
+        }
+
+        return (
+            <div id="weather" className={this.state.visible?'fadeIn':'fadeOut'} style={weatherStyle}>
+                <h1 id="current">Now</h1>
+                    {this.getWeather(this.state.weatherCurrent)}
+                <h2 id="forecast">{moment(this.state.weatherForecast.['dt_txt']).format('HH:mm a')}</h2>
+                    {this.getWeather(this.state.weatherForecast)}
+            </div>
+        );
+    }
+}
+
+export default Weather;
--- magicMirror/frontend/src/constants/index.js
+++ magicMirror/frontend/src/constants/index.js
@@ -1,4 +1,14 @@
-export const API_URL = "http://localhost:8000/api/";
-export const API_DATE_URL = "http://localhost:8000/api/date/";
-export const API_JOB_URL = "http://localhost:8000/api/job/";
-export const API_EVENT_URL = "http://localhost:8000/api/event/";
+export const WEATHER_API = 'https://api.openweathermap.org/data/2.5/';
+export const WEATHER_API_CITY = 'Cambridge,uk';
+export const WEATHER_API_KEY = '3c11533ea19fd775b9d59d0cf16089c5';
+export const WEATHER_ICON = {
+        '01d': 'Sun',
+        '01n': 'Moon',
+        '02d': 'Cloud_Sunny',
+        '02n': 'Cloud_Moon',
+        '03d': 'cloud','03n': 'Cloud','04d': 'Cloud','04n': 'Cloud',
+        '09d': 'Rain','09n': 'Rain','10d': 'Rain','10n': 'Rain',
+        '11d': 'Storm','11n': 'Storm',
+        '13d': 'Snow','13n': 'Snow',
+        '50d': 'Haze','50n': 'Haze'
+};
